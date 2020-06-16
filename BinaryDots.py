e5add9c80bd1ed47f2caa52c87ddcf7dbaa9e17f
Miguel Galvan <mga12987@gmail.com>
2020-06-15 19:59:38 -0700

    Simple Starter Window.Configured background color and added a button. Next task: Jump into another frame when clicking the button.

--- BinaryDots.py
+++ BinaryDots.py
@@ -1,1 +1,16 @@
-print('Hello Binary Dots')
+from tkinter import * 
+from tkinter.ttk import *
+from time import strftime 
+import tkinter as tk
+
+#simple window example 
+root = Tk()
+root.minsize(500,500) #minsize sets the size of the window 
+root.config(bg = "#a4f6f9") #this sets a different color to the background
+
+start_button = Button(root, text='Play Binary Dots')
+start_button.pack()
+start_button.place(relx=0.5, rely=0.5, anchor=CENTER)#places the intro button right in the center of the window
+
+
+mainloop()
